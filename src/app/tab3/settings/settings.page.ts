facc86c392fcc7b9492f2ae0385562c2f20ad4de
Dmytro <lytvyniuk.dima@gmail.com>
2021-11-05 15:57:43 +0200

    added dark mode support

--- src/app/tab3/settings/settings.page.ts
+++ src/app/tab3/settings/settings.page.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
-  styleUrls: ['./settings.page.scss'],
+  styleUrls: [ './settings.page.scss' ],
 })
 export class SettingsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  dakrMode = document.body.getAttribute('color-theme') === 'dark';
+  constructor() {
   }
 
+  ngOnInit() { }
+
+  toggleDarkTheme(shouldAdd): void {
+    document.body.setAttribute('color-theme', shouldAdd ? 'dark' : 'white');
+  }
 }
